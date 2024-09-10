2f64f371bf6caef089a8e394ba14713c101c63fd
Kim-Beomsoo <116559420+kbs1027@users.noreply.github.com>
2024-09-10 14:16:54 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -2,12 +2,5 @@ SELECT ai.ANIMAL_ID, ai.ANIMAL_TYPE, ai.NAME
 from ANIMAL_INS ai
 join ANIMAL_OUTS ao
 on ai.ANIMAL_ID = ao.ANIMAL_ID
-where ao.SEX_UPON_OUTCOME like '%Neutered%'
-or ao.SEX_UPON_OUTCOME like '%Spayed%'
-EXCEPT
-SELECT ai.ANIMAL_ID, ai.ANIMAL_TYPE, ai.NAME
-from ANIMAL_INS ai
-join ANIMAL_OUTS ao
-on ai.ANIMAL_ID = ao.ANIMAL_ID
-where ai.SEX_UPON_INTAKE like '%Neutered%'
-or ai.SEX_UPON_INTAKE like '%Spayed%'
+where (ao.SEX_UPON_OUTCOME like '%Neutered%' or ao.SEX_UPON_OUTCOME like '%Spayed%')
+and ai.SEX_UPON_INTAKE like '%Intact%'
